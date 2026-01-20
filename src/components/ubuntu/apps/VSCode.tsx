3f6857d6068ef648b840d8e7e0bba41e41ba712e
Ankit Garg <anki88520@gmail.com>
2026-01-20 13:32:07 +0530

    VS code

--- src/components/ubuntu/apps/VSCode.tsx
+++ src/components/ubuntu/apps/VSCode.tsx
@@ -1,11 +1,10 @@
 export function VSCode() {
   return (
     <iframe 
-      src="https://vscode.dev/github/AnKiT-GaRG2/my-ubuntu-portfolio" 
+      src="https://github1s.com/AnKiT-GaRG2/my-ubuntu-portfolio/tree/main/src" 
       frameBorder="0" 
       title="VS Code" 
       className="h-full w-full bg-gray-900"
-      allow="clipboard-read; clipboard-write"
     />
   );
 }
